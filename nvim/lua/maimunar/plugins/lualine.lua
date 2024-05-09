faa38fb8cb0340dcdee369021040f997a82a892c
Aleksandar Ivanov <metalcho17@gmail.com>
2024-05-09 21:10:54 +0200

    added full file name to lualine

--- nvim/lua/maimunar/plugins/lualine.lua
+++ nvim/lua/maimunar/plugins/lualine.lua
@@ -10,6 +10,13 @@ return {
         theme = "auto",
       },
       sections = {
+        lualine_c = {
+          {
+            "filename",
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+          },
+        },
         lualine_x = {
           {
             lazy_status.updates,
