d02e21cf148b30ee330a8e1cd897a13034d5f75c
Aleksandar Ivanov <metalcho17@gmail.com>
2024-05-09 21:38:06 +0200

    updated theme

--- nvim/lua/maimunar/plugins/colorscheme.lua
+++ nvim/lua/maimunar/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
- "akinsho/horizon.nvim", 
-   version = "*",
-   priority = 1000,
-   config = function()
-    vim.cmd.colorscheme('horizon')
-    vim.o.background = "dark" 
-   end
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+    })
+
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
