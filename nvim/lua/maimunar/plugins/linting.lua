f3db3c6c0d6c16bfe6e390fe7223fc1ee73a9a2e
Aleksandar Ivanov <metalcho17@gmail.com>
2024-05-09 21:37:34 +0200

    updates linters

--- nvim/lua/maimunar/plugins/linting.lua
+++ nvim/lua/maimunar/plugins/linting.lua
@@ -5,16 +5,29 @@ return {
   config = function()
     local lint = require("lint")
 
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
+      typescript = { "eslint_d", "eslint" },
+      javascript = { "eslint_d", "eslint" },
+      typescriptreact = { "eslint_d", "eslint" },
+      javascriptreact = { "eslint_d", "eslint" },
+      svelte = { "eslint_d", "eslint" },
       python = { "pylint" },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    local eslint = lint.linters.eslint_d
+
+    eslint.args = {
+      "--no-warn-ignored", -- <-- this is the key argument
+      "--format",
+      "json",
+      "--stdin",
+      "--stdin-filename",
+      function()
+        return vim.api.nvim_buf_get_name(0)
+      end,
+    }
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
