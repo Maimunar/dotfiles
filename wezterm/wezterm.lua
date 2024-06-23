4c65637f41394f70427a42afe0ef48c3c2135da7
Aleksandar Ivanov <metalcho17@gmail.com>
2024-06-24 00:01:40 +0200

    add a wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "tokyonight_night"
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 12
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
+}
+config.window_decorations = "RESIZE"
+-- and finally, return the configuration to wezterm
+return config
