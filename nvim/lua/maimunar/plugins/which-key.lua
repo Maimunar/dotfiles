ed65b5fb931730718eb72d7a1fbd665c5ab445fe
Aleksandar Ivanov <metalcho17@gmail.com>
2024-11-26 11:57:27 +0100

    which key config change

--- nvim/lua/maimunar/plugins/which-key.lua
+++ nvim/lua/maimunar/plugins/which-key.lua
@@ -6,8 +6,18 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    preset = "helix",
+    delay = 0,
+    win = {
+      height = {
+        max = math.huge,
+      },
+    },
+    icons = {
+      rules = false,
+      breadcrumb = " ", -- symbol used in the command line area that shows your active key combo
+      separator = "󱦰  ", -- symbol used between a key and it's label
+      group = "󰹍 ", -- symbol prepended to a group
+    },
   },
 }
