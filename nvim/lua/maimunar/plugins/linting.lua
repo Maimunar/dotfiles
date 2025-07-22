54544296b1304328cdb731882e522801810e5b3a
Aleksandar Ivanov <metalcho17@gmail.com>
2025-07-22 16:05:32 +0200

    adds marks and fixes linting

--- nvim/lua/maimunar/plugins/linting.lua
+++ nvim/lua/maimunar/plugins/linting.lua
@@ -5,32 +5,16 @@ return {
   config = function()
     local lint = require("lint")
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
     lint.linters_by_ft = {
-      typescript = { "eslint_d", "eslint" },
-      javascript = { "eslint_d", "eslint" },
-      typescriptreact = { "eslint_d", "eslint" },
-      javascriptreact = { "eslint_d", "eslint" },
-      svelte = { "eslint_d", "eslint" },
+      typescript = { "eslint_d" },
+      javascript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
       python = { "pylint" },
     }
 
-    local eslint = lint.linters.eslint_d
-
-    eslint.args = {
-      "--no-warn-ignored", -- <-- this is the key argument
-      "--format",
-      "json",
-      "--stdin",
-      "--stdin-filename",
-      function()
-        return vim.api.nvim_buf_get_name(0)
-      end,
-    }
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
         lint.try_lint()
       end,
