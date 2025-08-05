3fb6a7d6aff9fe32bdda7086485a32509a3cb441
Aleksandar Ivanov <metalcho17@gmail.com>
2025-08-05 12:33:35 +0200

    simplified lualine

--- nvim/lua/maimunar/plugins/lualine.lua
+++ nvim/lua/maimunar/plugins/lualine.lua
@@ -3,34 +3,31 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-    local lazy_status = require("lazy.status")
 
     local colors = require("tokyonight.colors").setup({ transform = true })
     local myTheme = require("lualine.themes.tokyonight")
     myTheme.normal.c.bg = colors.bg
     myTheme.inactive.c.bg = colors.bg
+    myTheme.normal.x = { fg = "#7aa2f7", bg = colors.bg }
 
     lualine.setup({
       options = {
         theme = myTheme,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_c = {
+        lualine_c = {},
+        lualine_x = {
           {
             "filename",
             file_status = true, -- displays file status (readonly status, modified status)
             path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
           },
-        },
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "filetype" },
           { "searchcount" },
         },
+        lualine_y = {},
+        lualine_z = {},
       },
     })
   end,
